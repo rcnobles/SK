e9b546fcbe6780df80c546165f9675eb913e9833
AntHoneyLam <47013152+AntHoneyLam@users.noreply.github.com>
2019-10-11 20:30:32 -0700

    Create Enter.cs

---
+++ RoomTransitions/Enter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Should be child function of Exit
+public class Enter : MonoBehaviour
+{
+    private string enterPassword;
+
+    private void Start()
+    {
+        // scenePassword is same as enterPassword
+        if (PlayerMovement.instance.scenePassword == enterPassword)
+        {
+            // entrance position
+            PlayerMovement.instance.transform.position = transform.position;
+            Debug.Log("Room Entered");
+        }
+        else
+        {
+            Debug.Log("Could not Enter Room");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
