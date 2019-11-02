fd68e5b9a433a1b068635925a21d5738ba88fa5d
rcnobles <36048441+rcnobles@users.noreply.github.com>
2019-11-01 20:43:38 -0700

    Update bear.cs

--- New Unity Project/Assets/Scripts/bear.cs
+++ New Unity Project/Assets/Scripts/bear.cs
@@ -2,7 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FollowerState{
+    idle,
+    walk,
+}
 public class bear : MonoBehaviour{
+    public FollowerState currentState;
+    private Rigidbody2D myRigidbody;
     // Target variable
     public Transform target;
     // Inside chase radius the bear will follow the player
@@ -12,24 +18,68 @@ public class bear : MonoBehaviour{
     public Transform homePosition;
     public string followerName;
     public float moveSpeed;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start(){
+        currentState = FollowerState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         // Where the bear needs to moving towards
         target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update(){
+    void FixedUpdate(){
         CheckDistance();
     }
 
     void CheckDistance(){
         // Only move towards player if inside of radius.
-        if(Vector3.Distance(target.position,
+        if (Vector3.Distance(target.position,
                             transform.position) <= outerChaseRadius
-                            && Vector3.Distance(target.position,transform.position) > innerChaseRadius){
-            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            && Vector3.Distance(target.position,transform.position) > innerChaseRadius){
+            if (currentState == FollowerState.idle || currentState == FollowerState.walk ){
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                changeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+                ChangeState(FollowerState.walk);
+                anim.SetBool("wakeUp", true);
+            }
+        }else if (Vector3.Distance(target.position,
+                            transform.position) > outerChaseRadius){
+            anim.SetBool("wakeUp", false);
+        } 
+
+    }
+
+    private void SetAnimFloat(Vector2 setVector){
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+    private void changeAnim(Vector2 direction){
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)){
+            if (direction.x > 0){
+                // Right
+                SetAnimFloat(Vector2.right);
+            }else if (direction.x < 0){
+                // Left
+                SetAnimFloat(Vector2.left);
+            }
+        }else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y)){
+            if (direction.x > 0){
+                // Up
+                SetAnimFloat(Vector2.up);
+            }else if (direction.x < 0){
+                // Down
+                SetAnimFloat(Vector2.down);
+            }
+        }
+    }
+    private void ChangeState(FollowerState newState){
+        if (currentState != newState)
+        {
+            currentState = newState;
         }
     }
 }
